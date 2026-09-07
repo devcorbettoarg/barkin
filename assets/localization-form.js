fb1e6d9ecf4cbfeb925072ce0ad0359dc5d129e1
DevCorbetto-tvp <gabriel.corbetto@vnstudios.com>
2026-09-07 10:26:36 -0500

    s

--- assets/localization-form.js
+++ assets/localization-form.js
@@ -47,8 +47,8 @@ if (!customElements.get('localization-form')) {
           this.elements.search.setAttribute('aria-activedescendant', '');
         }
         document.body.classList.remove('overflow-hidden-mobile');
-        document.querySelector('.menu-drawer').classList.remove('country-selector-open');
-        this.header.preventHide = false;
+        document.querySelector('.menu-drawer')?.classList.remove('country-selector-open');
+        if (this.header) this.header.preventHide = false;
       }
 
       onContainerKeyDown(event) {
@@ -122,10 +122,10 @@ if (!customElements.get('localization-form')) {
         if (this.elements.search && this.mql.matches) {
           this.elements.search.focus();
         }
-        if (this.hasAttribute('data-prevent-hide')) {
+        if (this.header && this.hasAttribute('data-prevent-hide')) {
           this.header.preventHide = true;
         }
-        document.querySelector('.menu-drawer').classList.add('country-selector-open');
+        document.querySelector('.menu-drawer')?.classList.add('country-selector-open');
       }
 
       closeSelector(event) {
